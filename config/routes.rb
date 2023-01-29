b4dc5d228c17755b06f5552c9eb5a80dcd5bcedd
Tácio Nery <tacio.nery@indiecampers.com>
2023-01-29 21:09:59 +0000

    configuring swagger and rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   post 'user', to: 'users#create'
   post 'login', to: 'sessions#create'
 
